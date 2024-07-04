5794d13ea6fb85e9c7cac9f864e53bbb3a267029
vMohammad24 <mohammad.s.alqadi@gmail.com>
2024-07-04 17:30:37 +0300

    fixed spotify

--- src/util/urls.ts
+++ src/util/urls.ts
@@ -2,7 +2,7 @@ import commandHandler from ".."
 
 export function getRedirectURL(platform: 'discord' | 'spotify') {
     const { prodMode } = commandHandler;
-    if (platform === 'spotify' && !prodMode) return process.env.REDIRECT_SPOTIFY_DEV!
+    if (platform === 'spotify') return prodMode ? process.env.REDIRECT_SPOTIFY_PROD! : process.env.REDIRECT_SPOTIFY_DEV!
     return (prodMode ? process.env.REDIRECT_PROD! : process.env.REDIRECT_DEV!).replace('{platform}', platform)
 }
 
