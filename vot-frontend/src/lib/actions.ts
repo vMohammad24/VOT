338a6cffd4746aeff9285244806e79db99590048
vMohammad24 <mohammad.s.alqadi@gmail.com>
2024-07-05 07:01:16 +0300

    removed the console.logs

--- vot-frontend/src/lib/actions.ts
+++ vot-frontend/src/lib/actions.ts
@@ -5,10 +5,10 @@ import { apiUrl, getApiUrl } from "./utils";
 
 export const submitTicketSettings = async (data: any, guildId: string, token: string) => {
     const user = await prisma.user.findUnique({ where: { token: token } });
-    if (!user) return console.log({ error: "User not found" });
+    if (!user) return;//console.log({ error: "User not found" });
     const guild = await prisma.guild.findUnique({ where: { id: guildId }, include: { admins: true } });
-    if (!guild) return console.log({ error: "Guild not found" });
-    if (!guild.admins.map(a => a.id).includes(user.id)) return console.log({ error: user.name + " is not an admin of this guild" });
+    if (!guild) return;// console.log({ error: "Guild not found" });
+    if (!guild.admins.map(a => a.id).includes(user.id)) return;// console.log({ error: user.name + " is not an admin of this guild" });
     const oldSettings = (await prisma.ticketSettings.findFirst({ where: { guildId: guildId } }));
     const actualData = {
         categoryId: data.category,
@@ -42,9 +42,9 @@ export const submitTicketSettings = async (data: any, guildId: string, token: st
 
 export const submitWelcomeSettings = async (data: any, guildId: string, token: string) => {
     const user = await prisma.user.findUnique({ where: { token: token } });
-    if (!user) return console.log({ error: "User not found" });
+    if (!user) return;// console.log({ error: "User not found" });
     const guild = await prisma.guild.findUnique({ where: { id: guildId, admins: { some: { id: user.id } } }, include: { admins: true } });
-    if (!guild) return console.log({ error: "Guild not found" });
+    if (!guild) return; //console.log({ error: "Guild not found" });
     const oldSettings = (await prisma.welcomeSettings.findFirst({ where: { guildId: guildId } }));
     const actualData = {
         channelId: data.channel,
