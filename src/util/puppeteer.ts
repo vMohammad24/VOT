ccb128647918cb6864f2dd20531de384bc6ac51d
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-09-27 22:15:41 +0300

    optimized some commands

--- src/util/puppeteer.ts
+++ src/util/puppeteer.ts
@@ -1,15 +1,17 @@
-import puppeteer from 'puppeteer-extra';
+import puppeteer, { VanillaPuppeteer } from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
+
+let browser: ReturnType<VanillaPuppeteer["launch"]> | undefined = undefined;
+
 export async function launchPuppeteer() {
-	return puppeteer.use(StealthPlugin()).launch({
+	if (!browser) browser = puppeteer.use(StealthPlugin()).launch({
 		headless: true,
 		defaultViewport: {
 			width: 1920,
 			height: 1080,
-			deviceScaleFactor: 1,
-			isLandscape: true,
 		},
 		args: ['--no-sandbox', '--enable-features=WebContentsForceDark'],
 	});
+	return browser;
 }
