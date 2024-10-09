bca43c80c608ffb044c7367a9790ccc63e6430aa
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-10-09 19:58:42 +0300

    /userinfo now shows reveiws

--- src/commands/misc/userinfo.ts
+++ src/commands/misc/userinfo.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { write } from "bun";
-import { ApplicationCommandOptionType, EmbedBuilder, GuildMember } from "discord.js";
+import {
+    ActionRowBuilder,
+    ApplicationCommandOptionType,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder,
+    GuildMember,
+    User,
+} from "discord.js";
+import { nanoid } from "nanoid/non-secure";
 import { join } from 'path';
 import removeMarkdown from 'remove-markdown';
 import ICommand from "../../handler/interfaces/ICommand";
@@ -16,8 +25,9 @@ export default {
         description: 'User to get information about',
         required: false
     }],
-    execute: async ({ args, member, interaction, handler }) => {
-        const user = args.get('user') as GuildMember || member;
+    type: 'all',
+    execute: async ({ args, user: usr, interaction, handler, message }) => {
+        const user = (args.get('user') as User | GuildMember) || usr;
         await interaction?.deferReply();
         const res = await axios.get('https://us-atlanta2.evade.rest/users/' + user.id);
         const { data } = res;
@@ -37,30 +47,27 @@ export default {
                 badge.emoji = (await addEmoji(path, ems))?.toString()!;
             }
         })()
-        return {
-            embeds: [new EmbedBuilder()
-                .setTitle('User Information')
-                .setThumbnail(user.user.displayAvatarURL())
-                .setAuthor({ name: user.user.tag, iconURL: user.user.displayAvatarURL(), url: `https://discord.com/users/${user.id}` })
-                // .setDescription(`
-                //     **ID**: ${user.id}
-                //     **Badges**: ${badges.map(badge => badge.emoji).join('')}
-                //     **Bio**: ${removeMarkdown(bio) || 'None'}
-                //     **Roles**: ${user.roles.cache.filter(role => (!role.managed && (role.id != user.guild.roles.everyone.id))).map(role => role.toString()).join(' ')}
-                //     **Joined**: ${user.joinedTimestamp ? `<t:${Math.round(user.joinedTimestamp! / 1000)}>` : 'Unknown'}
-                //     **Created**: <t:${Math.round(user.user.createdTimestamp / 1000)}>
-                //     `)
-                .setDescription(bio ? removeMarkdown(bio) : null)
-                .setFields([
-                    {
-                        name: 'ID',
-                        value: user.id,
-                    },
-                    {
-                        name: 'Badges',
-                        value: badges.map(badge => badge.emoji).join(''),
-                        inline: true
-                    },
+        const buttonId = nanoid();
+
+        const u = user instanceof GuildMember ? user.user : user;
+
+
+        const embed = new EmbedBuilder()
+            .setTitle('User Information')
+            .setThumbnail(u.displayAvatarURL())
+            .setAuthor({ name: u.tag, iconURL: u.displayAvatarURL(), url: `https://discord.com/users/${u.id}` })
+            .setDescription(bio ? removeMarkdown(bio) : null)
+            .setFields([
+                {
+                    name: 'ID',
+                    value: u.id,
+                },
+                {
+                    name: 'Badges',
+                    value: badges.map(badge => badge.emoji).join(''),
+                    inline: true
+                },
+                ...(user instanceof GuildMember ? [
                     {
                         name: 'Roles',
                         value: user.roles.cache.filter(role => (!role.managed && (role.id != user.guild.roles.everyone.id))).map(role => role.toString()).join(' '),
@@ -70,17 +77,54 @@ export default {
                         name: 'Joined',
                         value: user.joinedTimestamp ? `<t:${Math.round(user.joinedTimestamp! / 1000)}>` : 'Unknown',
                         inline: true
-                    },
-                    {
-                        name: 'Created',
-                        value: `<t:${Math.round(user.user.createdTimestamp / 1000)}>`,
-                        inline: true
                     }
-                ])
+                ] : []),
+                {
+                    name: 'Created',
+                    value: `<t:${Math.round(u.createdTimestamp / 1000)}>`,
+                    inline: true
+                }
+            ])
+            .setColor('Random')
+            .setTimestamp();
+        const sentMessage = message ? await message.reply({
+            embeds: [embed],
+            allowedMentions: { repliedUser: true },
+            components: [
+                new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder().setLabel('View reviews').setStyle(ButtonStyle.Primary).setCustomId(buttonId)
+                )
+            ]
+        }) : await interaction!.editReply({
+            embeds: [embed],
+            allowedMentions: { repliedUser: true },
+            components: [
+                new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder().setLabel('View reviews').setStyle(ButtonStyle.Primary).setCustomId(buttonId)
+                )
+            ]
+        });
+        const collector = sentMessage?.createMessageComponentCollector({ filter: i => i.customId === buttonId });
+        collector?.on('collect', async i => {
+            const reviewsRes = await axios.get(`https://manti.vendicated.dev/api/reviewdb/users/${user.id}/reviews`)
+            const reviews: any[] = reviewsRes.data.reviews;
+            const embed = new EmbedBuilder()
+                .setTitle('Reviews')
+                .setAuthor({ name: u.tag, iconURL: u.displayAvatarURL(), url: `https://discord.com/users/${user.id}` })
+                // .setDescription(embedDesc)
                 .setColor('Random')
-                .setTimestamp()
-            ],
-            allowedMentions: { repliedUser: true }
-        }
+                .setTimestamp();
+            if (reviews) {
+                // remove the first one
+                reviews.shift();
+                embed.setDescription(reviews.map(review => `- **${review.sender.username}** - ${review.comment}`).join('\n'))
+            }
+            i.reply({
+                embeds: [embed
+                ],
+                ephemeral: true,
+                allowedMentions: { repliedUser: true }
+            })
+        })
     }
 } as ICommand
