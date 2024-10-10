39626fa49edf2718d369cd57d5f0a7148cbcfacf
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-10-10 12:42:20 +0300

    /userinfo now shows connections

--- src/commands/misc/userinfo.ts
+++ src/commands/misc/userinfo.ts
@@ -11,9 +11,46 @@ import {
 } from "discord.js";
 import { nanoid } from "nanoid/non-secure";
 import { join } from 'path';
-import removeMarkdown from 'remove-markdown';
 import ICommand from "../../handler/interfaces/ICommand";
 import { addEmoji } from "../../util/emojis";
+
+
+interface Decoration {
+    id: string;
+    name: string;
+    animated: boolean;
+}
+
+interface Badge {
+    name: string;
+    description: string;
+    emoji: string;
+}
+
+interface Connection {
+    type: string[];
+    name: string;
+    id: string;
+    metadata: any;
+    url: string | null;
+}
+
+interface UserInfo {
+    id: string;
+    name: string;
+    username: string;
+    discriminator: string;
+    decoration: Decoration;
+    avatar_url: string;
+    banner_url: string | null;
+    flags: number;
+    created_at: string;
+    bot: boolean;
+    bio: string;
+    badges: Badge[];
+    connections: Connection[];
+}
+
 export default {
     name: 'userinfo',
     aliases: ['user', 'whois', 'ui'],
@@ -26,27 +63,29 @@ export default {
         required: false
     }],
     type: 'all',
-    execute: async ({ args, user: usr, interaction, handler, message }) => {
-        const user = (args.get('user') as User | GuildMember) || usr;
+    execute: async ({ args, user: usr, member, interaction, handler, message }) => {
+        const user = (args.get('user') as GuildMember | User) || member || usr;
         await interaction?.deferReply();
         const res = await axios.get('https://us-atlanta2.evade.rest/users/' + user.id);
-        const { data } = res;
-        const { bio } = data;
+        const data = res.data as UserInfo;
+        const { bio, connections } = data;
         const badges: {
             name: string;
             description: string;
             emoji: string;
         }[] = data.badges;
-        await (async () => {
-            const ems = await handler.client.application?.emojis.fetch();
-            for (const badge of badges) {
-                const emojiId = badge.emoji.match(/:(\d+)>$/)?.[1];
-                const res = await axios.get(`https://cdn.discordapp.com/emojis/${emojiId}.png?size=512&quality=lossless`, { responseType: 'arraybuffer' });
-                const path = join(import.meta.dir, '..', '..', '..', 'assets', 'emojis', `${badge.name}.png`);
-                await write(path, res.data);
-                badge.emoji = (await addEmoji(path, ems))?.toString()!;
-            }
-        })()
+        if (badges && badges.length > 0) {
+            await (async () => {
+                const ems = await handler.client.application?.emojis.fetch();
+                for (const badge of badges) {
+                    const emojiId = badge.emoji.match(/:(\d+)>$/)?.[1];
+                    const res = await axios.get(`https://cdn.discordapp.com/emojis/${emojiId}.png?size=512&quality=lossless`, { responseType: 'arraybuffer' });
+                    const path = join(import.meta.dir, '..', '..', '..', 'assets', 'emojis', `${badge.name}.png`);
+                    await write(path, res.data);
+                    badge.emoji = (await addEmoji(path, ems))?.toString()!;
+                }
+            })()
+        }
         const buttonId = nanoid();
 
         const u = user instanceof GuildMember ? user.user : user;
@@ -56,16 +95,24 @@ export default {
             .setTitle('User Information')
             .setThumbnail(u.displayAvatarURL())
             .setAuthor({ name: u.tag, iconURL: u.displayAvatarURL(), url: `https://discord.com/users/${u.id}` })
-            .setDescription(bio ? removeMarkdown(bio) : null)
+            .setDescription(`
+                ${(badges && badges.length > 0) ? `**Badges**:\n### ${badges.map(badge => badge.emoji).join('')}` : ''}
+                ${(connections && connections.length > 0) ? `**Connections**:
+                ${connections.map(connection => (
+                connection.url ? `[${connection.name}](${connection.url})`
+                    : `${connection.type.includes('domain') ?
+                        `[${connection.name}](https://${connection.name})` :
+                        connection.name} ${connection.type.find(a => a.includes('unknown')) ?
+                            '' : `(${connection.type[0]})`}`)).join('\n')}`
+                    : ''}
+
+                ${(bio) ? `**Bio**:\n${bio}` : ''}
+                `)
             .setFields([
                 {
                     name: 'ID',
                     value: u.id,
-                },
-                {
-                    name: 'Badges',
-                    value: badges.map(badge => badge.emoji).join(''),
-                    inline: true
+                    inline: true,
                 },
                 ...(user instanceof GuildMember ? [
                     {
@@ -85,7 +132,7 @@ export default {
                     inline: true
                 }
             ])
-            .setColor('Random')
+            .setColor(u.hexAccentColor || 'Random')
             .setTimestamp();
         const sentMessage = message ? await message.reply({
             embeds: [embed],
@@ -112,12 +159,12 @@ export default {
                 .setTitle('Reviews')
                 .setAuthor({ name: u.tag, iconURL: u.displayAvatarURL(), url: `https://discord.com/users/${user.id}` })
                 // .setDescription(embedDesc)
-                .setColor('Random')
+                .setColor(u.hexAccentColor || 'Random')
                 .setTimestamp();
             if (reviews) {
                 // remove the first one
                 reviews.shift();
-                if (reviews.length === 0) return i.reply({ content: 'No reviews found', ephemeral: true });
+                if (reviews.length <= 0) return i.reply({ content: 'No reviews found', ephemeral: true });
                 embed.setDescription(reviews.map(review => `- **${review.sender.username}** - ${review.comment}`).join('\n'))
             }
             i.reply({
