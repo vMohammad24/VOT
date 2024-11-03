15c5b2ca0c86615ed8f7aa8daf37708bf6cac417
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-11-03 21:49:06 +0300

    [silent]

--- src/handler/ListenerHandler.ts
+++ src/handler/ListenerHandler.ts
@@ -18,8 +18,6 @@ export default class ListenerHandler {
 			absolute: false,
 			cwd: listenersDir,
 		})) {
-			const fileName = file.split('/').pop();
-			if (fileName?.startsWith('_')) return;
 			const listener = await import(join(listenersDir, file));
 			this.listeners.push(listener.default as IListener);
 			listener.default.execute(this.handler);
