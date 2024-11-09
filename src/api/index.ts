d20978af56d04a5e786f2d0730e584b3bd3d4a01
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-11-09 18:56:28 +0300

    - Removed /lookup ammo.lol - Added /lookup guns.lol

--- src/api/index.ts
+++ src/api/index.ts
@@ -108,7 +108,7 @@ elysia.post('/askDDG', async ({ body, headers, set }) => {
 		set.status = 401;
 		return { error: 'Unauthorized' };
 	};
-	const res = await askDDG(query, model);
+	const res = await askDDG(query, model as "gpt-4o-mini" | "claude-3-haiku-20240307" | "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo" | "mistralai/Mixtral-8x7B-Instruct-v0.1");
 	return res;
 }, {
 	body: t.Object({
