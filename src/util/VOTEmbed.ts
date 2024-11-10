cf9d88ef53ac64b137b0a74653686f57f10c54ca
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-11-10 19:31:55 +0300

    - Added back /lookup ammo.lol

--- src/util/VOTEmbed.ts
+++ src/util/VOTEmbed.ts
@@ -1,10 +1,21 @@
+import { loadImage } from '@napi-rs/canvas';
 import { EmbedBuilder, EmbedData } from 'discord.js';
 import commandHandler from '..';
+import { getTwoMostUsedColors } from './util';
 let votIcon: string | undefined = undefined;
 class VOTEmbed extends EmbedBuilder {
     constructor(data?: EmbedData) {
         super(data);
         if (!votIcon) commandHandler.client.user?.displayAvatarURL({ size: 1024 });
+    }
+
+    async dominant() {
+        const json = this.toJSON();
+        if (!json.color && json.thumbnail) {
+            const image = await loadImage(json.thumbnail.url);
+            this.setColor(getTwoMostUsedColors(image)[0]);
+        }
+        return this;
     }
 
     toJSON() {
@@ -15,6 +26,11 @@ class VOTEmbed extends EmbedBuilder {
         if (!json.footer) {
             json.footer = {
                 text: 'VOT',
+                icon_url: votIcon,
+            }
+        } else if (!json.author) {
+            json.author = {
+                name: 'VOT',
                 icon_url: votIcon,
             }
         }
