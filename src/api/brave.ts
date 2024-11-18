28da9599275ed6501d41a7a7e7bf7669d5b2d73f
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-11-18 20:10:41 +0300

    [silent] API docs improvements

--- src/api/brave.ts
+++ src/api/brave.ts
@@ -1,19 +1,13 @@
 import type Elysia from 'elysia';
 import { t } from 'elysia';
 import TurndownService from 'turndown';
-import { chatllm, searchBrave } from '../util/brave';
-import { checkKey } from './apiUtils';
+import { chatllm, searchBrave, searchBraveSuggest } from '../util/brave';
 const turndown = new TurndownService();
 export default (server: Elysia<'brave'>) => {
 	server.post(
 		'/ask',
 		async ({ body, headers, set }) => {
 			const { query } = body;
-			const { authorization } = headers;
-			if (!(await checkKey(authorization))) {
-				set.status = 401;
-				return { error: 'Unauthorized' };
-			}
 			const search = await searchBrave(query);
 			const llm = await chatllm(search.data.body.response.chatllm);
 			return llm;
@@ -25,6 +19,39 @@ export default (server: Elysia<'brave'>) => {
 			headers: t.Object({
 				authorization: t.String(),
 			}),
+			detail: {
+				description: 'Ask a question using Brave search',
+			},
+			response: t.Object({
+				raw_response: t.String(),
+				references: t.Array(t.Any()),
+				urls: t.Array(t.String()),
+				images: t.Array(t.Object({
+					src: t.String(),
+					text: t.String(),
+					page_url: t.String(),
+					query_text: t.String(),
+					click_url: t.Union([t.String(), t.Null()]),
+				})),
+				qa: t.Array(t.Object({
+					answer: t.String(),
+					score: t.Number(),
+					href: t.Union([t.String(), t.Null()]),
+				})),
+				entities: t.Array(t.Any()),
+				main_entity: t.Union([t.String(), t.Null()]),
+				main_entity_infobox: t.Union([t.Any(), t.Null()]),
+				predicate: t.Union([t.Any(), t.Null()]),
+				context: t.Array(t.Any()),
+				context_urls: t.Array(t.String()),
+				context_results: t.Array(t.Object({
+					url: t.String(),
+					title: t.String(),
+					hostname: t.String(),
+					favicon: t.String(),
+				})),
+				followups: t.Optional(t.Array(t.String()))
+			}),
 		},
 	);
 
@@ -32,11 +59,6 @@ export default (server: Elysia<'brave'>) => {
 		'/search',
 		async ({ query: s, headers, set }) => {
 			const { query } = s;
-			const { authorization } = headers;
-			if (!(await checkKey(authorization))) {
-				set.status = 401;
-				return { error: 'Unauthorized' };
-			}
 			const search = await searchBrave(query);
 			const { results } = search.data.body.response.web;
 			return results.map((result) => ({
@@ -52,6 +74,33 @@ export default (server: Elysia<'brave'>) => {
 			headers: t.Object({
 				authorization: t.String(),
 			}),
+			detail: {
+				description: 'Search for a query using Brave search',
+			},
+			response: t.Array(t.Object({
+				title: t.String(),
+				description: t.String(),
+				url: t.String(),
+			}))
+		},
+	);
+
+	server.get(
+		'/suggest',
+		async ({ query: { query }, headers, set }) => {
+			return await searchBraveSuggest(query);
+		},
+		{
+			query: t.Object({
+				query: t.String(),
+			}),
+			headers: t.Object({
+				authorization: t.String(),
+			}),
+			detail: {
+				description: 'Get search suggestions from Brave',
+			},
+			response: t.Array(t.String())
 		},
 	);
 };
