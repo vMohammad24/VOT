1e3a8fe89303e77e8e08d012c1b8c67ab3bb1d09
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2024-12-20 19:32:35 +0300

    [silent] Buy premium

--- src/util/emojis.ts
+++ src/util/emojis.ts
@@ -89,7 +89,7 @@ export async function initEmojis() {
 export async function addEmojiByURL(name: string, url: string, ems: Collection<string, ApplicationEmoji> | undefined) {
 	if (emojis.has(name)) return emojis.get(name)!;
 	const res = await axios.get(url, { responseType: 'arraybuffer' });
-	const path = join(import.meta.dir, '..', '..', '..', 'assets', 'emojis', `${name}.png`);
+	const path = join(import.meta.dir, '..', '..', 'assets', 'emojis', `${name}.png`);
 	await write(path, res.data);
 	const emoji = await addEmoji(path, ems);
 	return emoji;
