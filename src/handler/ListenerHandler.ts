c4a7a07118c766f3a5704f419a97ccd3e7f02ffc
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2025-03-14 23:40:02 +0300

    [silent] https://discord.gg/X42fBGVRtR

--- src/handler/ListenerHandler.ts
+++ src/handler/ListenerHandler.ts
@@ -1,4 +1,5 @@
 import type { Glob } from 'bun';
+import { join } from 'path';
 import type CommandHandler from '.';
 
 export default class ListenerHandler {
@@ -12,18 +13,15 @@ export default class ListenerHandler {
 	}
 
 	public async initListeners(listenersDir: string) {
-		const files = [...this.glob.scanSync({
-			absolute: true,
+		for await (const file of this.glob.scan({
+			absolute: false,
 			cwd: listenersDir,
-		})];
-		const s = await Promise.all(files.map(file => {
-			return import(file).then((listener) => {
-				if (this.handler.verbose) this.handler.logger.info(`Initialized listener ${listener.default.name}`);
-				this.listeners.push(listener.default as IListener);
-				listener.default.execute(this.handler);
-			});
-		}))
-		if (this.handler.verbose) this.handler.logger.info(`Initialized ${s.length}/${this.listeners.length} listeners`);
+		})) {
+			const listener = await import(join(listenersDir, file));
+			this.listeners.push(listener.default as IListener);
+			listener.default.execute(this.handler);
+			if (this.handler.verbose) this.handler.logger.info(`Initialized listener ${listener.default.name}`);
+		}
 	}
 }
 
